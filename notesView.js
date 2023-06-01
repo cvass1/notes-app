0c514dc5e1fe025847424a6edbbc774028e59747
Caroline Evans <carolineevans261@gmail.com>
2023-06-01 12:02:11 +0100

    added addNote() feature to view

--- notesView.js
+++ notesView.js
@@ -3,15 +3,34 @@ class NotesView {
     constructor(model) {
         this.model = model;
         this.mainContainerEl = document.querySelector('#main-container');
+        this.inputEl = document.querySelector('#user-input');
+        this.addNoteButton = document.querySelector('#add-note-button');
+        this.addNoteButton.addEventListener('click', () =>{
+            this.addNewNote();
+            this.inputEl.value = "";
+        })
+    }
+
+    addNewNote() {
+        this.model.addNote(this.inputEl.value);
+        this.displayNotes();
     }
 
     displayNotes() {
+        this.clearNotes()
         this.model.getNotes().forEach( note => {
             let newDiv = document.createElement('div');
             newDiv.textContent = note;
             newDiv.className = 'note';
             this.mainContainerEl.append(newDiv);
         });
+    }
+
+    clearNotes() {
+        const allNoteDivs = document.querySelectorAll('div.note');
+        allNoteDivs.forEach((div) => {
+            div.remove();
+        })
     }
 
 };
--- notesView.test.js
+++ notesView.test.js
@@ -10,14 +10,52 @@ const NotesModel = require('./notesModel');
 describe('Notes View', () => {
     it('displays all the notes', () =>{
         document.body.innerHTML = fs.readFileSync('./index.html')
-        model = new NotesModel();
-        view = new NotesView(model);
+        const model = new NotesModel();
+        const view = new NotesView(model);
 
         model.addNote("test note");
         model.addNote("second test note");
         view.displayNotes();
-        let allNoteDivs = document.querySelectorAll('div.note');
+        const allNoteDivs = document.querySelectorAll('div.note');
         expect(allNoteDivs.length).toBe(2);
         
+    });
+
+    it('allows user to input a new note', () =>{
+        document.body.innerHTML = fs.readFileSync('./index.html')
+        const model = new NotesModel();
+        const view = new NotesView(model);
+
+        const addNoteButton = document.querySelector('#add-note-button');
+        const inputEl = document.querySelector('#user-input');
+        inputEl.value = "this is a user input test";
+        addNoteButton.click();
+
+        const allNoteDivs = document.querySelectorAll('div.note');
+        expect(allNoteDivs.length).toBe(1);
+        expect(allNoteDivs[0].textContent).toBe("this is a user input test");
+
+    });
+
+    it('allows user to input multiple notes', () =>{
+        document.body.innerHTML = fs.readFileSync('./index.html')
+        const model = new NotesModel();
+        const view = new NotesView(model);
+
+        const addNoteButton = document.querySelector('#add-note-button');
+        const inputEl = document.querySelector('#user-input');
+        
+        inputEl.value = "this is the first user input test";
+        addNoteButton.click();
+        
+        inputEl.value = "this is the second user input test";
+        addNoteButton.click();
+
+        const allNoteDivs = document.querySelectorAll('div.note');
+        expect(allNoteDivs.length).toBe(2);
+        expect(allNoteDivs[0].textContent).toBe("this is the first user input test");
+        expect(allNoteDivs[1].textContent).toBe("this is the second user input test");
+        expect(inputEl.value).toBe("");
+
     });
 });
